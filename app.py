c756704a901389cbc39cab4d23c337cc0ad8254b
Nandha Kumar Palanisamy <nandha.palanisamy@solitontech.local>
2023-07-03 16:30:37 +0530

    Like Dislike prototype

--- app.py
+++ app.py
@@ -1,33 +1,40 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "First Web App here! Hellooo :-) "
 
-@app.route("/next/")
+@app.route("/like/")
 def next():
-    import os
 
+    logme("like")
+
+    return "Like recorded"
+
+@app.route("/dislike/")
+def next():
+    
+    logme("dislike")
+    return "DisLike recorded"
+
+
+def logme(message):
     # Get the current user's home directory
-    home_dir = os.path.expanduser("~")
+    home_dir = "/home/logs"
 
     # Define the file name and path within the user's home directory
-    file_name = "example.txt"
+    file_name = message +".txt"
+
     file_path = os.path.join(home_dir, file_name)
-
+    if not os.path.exists(file_path):
+        os.mkdir(file_path)
     # Write content to the file
-    content = "This is an example file."
+    content = "This is a "+ message
     with open(file_path, "w") as file:
-        file.write(content)
-
+        file.write(content+ "\n")
     print("File stored at:", file_path)
-
-    return "This is cool! What' next?"
-
-
-
 
 if __name__== '__main__':
     app.run()
