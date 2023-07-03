cf0f711361917312cccc44f29ce2d35f46d1605b
Nandha Kumar Palanisamy <nandha.palanisamy@solitontech.local>
2023-07-03 16:42:59 +0530

    Revamped to Simple

--- app.py
+++ app.py
@@ -22,14 +22,13 @@ def dislike():
 
 def logme(message):
     # Get the current user's home directory
-    home_dir = "//home//logs"
+    home_dir = "/home/logs"
 
     # Define the file name and path within the user's home directory
     file_name = message +".txt"
 
     file_path = os.path.join(home_dir, file_name)
-    if not os.path.exists(file_path):
-        os.mkdir(file_path)
+    
     # Write content to the file
     content = "This is a "+ message
     with open(file_path, "w") as file:
