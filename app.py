79b39de310f8723c4adafda8cac1198e51f9f08d
Nandha Kumar Palanisamy <nandha.palanisamy@solitontech.local>
2023-07-03 15:56:18 +0530

    Trying to create a local file

--- app.py
+++ app.py
@@ -8,7 +8,26 @@ def index():
 
 @app.route("/next/")
 def next():
+    import os
+
+    # Get the current user's home directory
+    home_dir = os.path.expanduser("~")
+
+    # Define the file name and path within the user's home directory
+    file_name = "example.txt"
+    file_path = os.path.join(home_dir, file_name)
+
+    # Write content to the file
+    content = "This is an example file."
+    with open(file_path, "w") as file:
+        file.write(content)
+
+    print("File stored at:", file_path)
+
     return "This is cool! What' next?"
+
+
+
 
 if __name__== '__main__':
     app.run()
